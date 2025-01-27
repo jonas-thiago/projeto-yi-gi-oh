9bb5dfd37799a72adff3f05bed5622cb3b48c731
Jonas Thiago <146122741+jonas-thiago@users.noreply.github.com>
2025-01-27 14:52:28 -0300

    version6
    pego do site

--- src/js/index.js
+++ src/js/index.js
@@ -1,67 +1,45 @@
-/*
-
-OBJETIVO 1 - quando clicarmos na seta de avançar temos que mostrar o proximo 
-cartao da lista
-    - passo 1 - dar um jeito de pegar o elemento HTML da seta avancar
-    - passo 2 - dar um jeito de identificar o clique do usuário na seta avançar
-    - passo 3 - fazer aparecer o próximo cartão da lista 
-    - passo 4 - buscar o cartão que esta selecionado e esconder
-
-OBJETIVO 2 - quando clicarmos na seta de voltar temos que mostrar o cartão 
-anterior da lista
-    - passo 5 - dar um jeito de pegar o elemento HTML da seta voltar
-    - passo 6 - dar um jeito de identificar o clique do usuário  seta voltar
-    - passo 7 - fazer aparecer o cartão anterior da lista
-    - passo 8 - buscar o cartão que esta selecionado e esconder
-*/
-
-// Passo 1
-const btnAvancar = document.getElementById('btn-avancar');
-const cartoes = document.querySelectorAll('.cartao');
+const btnAvancar = document.getElementById("btn-avancar");
+const btnVoltar = document.getElementById("btn-voltar");
+const cartoes = document.querySelectorAll(".cartao");
 let cartaoAtual = 0;
-// Passo 5
-const btnVoltar = document.getElementById('btn-voltar')
-// Passo 2
 
-// Clicar pra virar a carta
 cartoes.forEach(cartao => {
-    cartao.addEventListener('click', function() {
-        const cartaVirada = cartao.querySelector('.carta-virada');
-        // virar o cartão
-        cartao.classList.toggle('virar');
-        // mostrar o fundo da carta
-        cartaVirada.classList.toggle('mostrar-fundo-carta');
+  cartao.addEventListener("click", function() {
+    const cartaVirada = cartao.querySelector(".carta-virada");
 
-        const descricao = cartao.querySelector('.descricao');
-        descricao.classList.toggle('esconder');
-    });            
+    // virar o cartão
+    cartao.classList.toggle("virar");
+    // mostrar o fundo da carta
+    cartaVirada.classList.toggle("mostrar-fundo-carta");
+
+    const descricao = cartao.querySelector(".descricao");
+    descricao.classList.toggle("esconder");
+  });
 });
 
-btnAvancar.addEventListener('click', function(){
-    if(cartaoAtual === cartoes.length -1) return;
-    // Passo 4 - Adicionar a classe selecionado
-    esconderCartaoSelecionado();
-    // Passo 3 
-    cartaoAtual++;
-    mostrarCartao(cartaoAtual);
-})
+btnAvancar.addEventListener("click", function () {
+  if (cartaoAtual === cartoes.length - 1) return;
 
-// Passo 6
-btnVoltar.addEventListener('click', function(){
-    if(cartaoAtual === 0) return;
-    // Passo 8 - Adicionar a classe selecionado
-    esconderCartaoSelecionado();
-    // Passo 7 
-    cartaoAtual--;
-    mostrarCartao(cartaoAtual);
-})
+  esconderCartaoSelecionado();
+
+  cartaoAtual++;
+  mostrarCartao(cartaoAtual);
+});
+
+btnVoltar.addEventListener("click", function () {
+  if (cartaoAtual === 0) return;
+
+  esconderCartaoSelecionado();
+
+  cartaoAtual--;
+  mostrarCartao(cartaoAtual);
+});
 
 function mostrarCartao(cartaoAtual) {
-    cartoes[cartaoAtual].classList.add('selecionado');
+  cartoes[cartaoAtual].classList.add("selecionado");
 }
 
 function esconderCartaoSelecionado() {
-    const cartaoSelecionado = document.querySelector('.selecionado');
-    cartaoSelecionado.classList.remove('selecionado');
+  const cartaoSelecionado = document.querySelector(".selecionado");
+  cartaoSelecionado.classList.remove("selecionado");
 }
-
